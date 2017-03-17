d2924dd882d68d232c3de59130656d6d543e9388
Kat Reinhart <kat.reinhart@gmail.com>
2017-03-17 14:27:57 -0700

    Debug refactor

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -11,48 +11,58 @@ var stateDefault = {
 var nextHobbyId = 1;
 var nextMovieId = 1;
 
-var reducer = (state = stateDefault, action) => {
-
-  switch (action.type) {
+var nameReducer = (state = 'Anonymous', action) => {
+  switch (action.type){
     case 'CHANGE_NAME':
-      return {
-        ...state,
-        name: action.name
-      }
+    return {
+      ...state,
+      name: action.name
+    }
+    default:
+      return state;
+  }
+}
+
+var hobbiesReducer = (state = [], action) => {
+  switch (action.type) {
     case 'ADD_HOBBY':
-      return{
-        ...state,
-        hobbies: [
-          ...state.hobbies, {
+      return [
+        ...state, {
             id: nextHobbyId++,
             hobby: action.hobby
-          }]
-      }
+          }
+      ]
     case 'REMOVE_HOBBY':
-      return {
-        ...state,
-        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-      }
+      return state.filter((hobby) => hobby.id !== action.id)
+
+    default:
+      return state;
+  }
+}
+
+var moviesReducer = (state = [], action) => {
+  switch (action.type) {
     case 'ADD_MOVIE':
-      return {
-        ...state,
-        movies: [
-          ...state.movies, {
+      return [
+          ...state, {
             id: nextMovieId++,
             title: action.title,
             genre: action.genre
           }
         ]
-      }
     case 'REMOVE_MOVIE':
-      return {
-        ...state,
-        movies: state.movies.filter((movie) => movie.id !== action.id)
-      }
+        return state.filter((movie) => movie.id !== action.id)
+
     default:
       return state;
   }
-};
+}
+
+var reducer = redux.combineReducers({
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: moviesReducer
+})
 
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
